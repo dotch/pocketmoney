d3ef9f17285d240a97da44a4fce88f9dd4a97d71
dotch <christian.x.weiss@gmail.com>
2015-06-13 12:11:58 +0200

    Initial Commit

---
+++ pocketmoney/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  pocketmoney
+//
+//  Created by cweiss on 13/06/15.
+//  Copyright (c) 2015 pocketmoney. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
