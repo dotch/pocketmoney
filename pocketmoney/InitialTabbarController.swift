c2fec8ca3273e0d9fc99df7df4980af01f9d6adc
Maksim <maksim.antin@gmail.com>
2015-06-13 15:11:47 +0200

    first flow with login and signup with parse

---
+++ pocketmoney/InitialTabbarController.swift
@@ -0,0 +1,24 @@
+//
+//  InitialTabbarController.swift
+//  pocketmoney
+//
+//  Created by Maksim Antin on 13.06.15.
+//  Copyright (c) 2015 pocketmoney. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class InitialTabbarController: UITabBarController {
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        println("UITAB called!")
+        //PFUser.logOut()
+        let currentUser = PFUser.currentUser()
+        if(currentUser == nil){
+            performSegueWithIdentifier("showLogin", sender: self)
+        }
+
+    }
+}
